5f606330232e5a5162ca03eff8032007336873ea
이준아 <oijoona@g.skku.edu>
2023-06-22 22:25:42 +0900

    23.06.16 main code(sw..)

---
+++ main.py
@@ -0,0 +1,15 @@
+import Function_Library as fl
+
+arduino_port = 'COM4'
+
+ser = fl.libARDUINO()
+
+comm = ser.init(arduino_port, 9600)
+
+while True:
+    input_value = input('아두이노로 전송할 저항 값 (0~255)')
+
+    comm.write(input_value.encode())
+
+    if input_value == 'q':
+        break
